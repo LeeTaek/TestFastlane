060d8902dc6a83f2eff0090c707f2c902aba2a29
이택성 <leetaek@itaegseong-ui-MacBookPro.local>
2025-03-14 14:40:54 +0900

    git init

---
+++ TestFastlane/TestFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneApp.swift
+//  TestFastlane
+//
+//  Created by 이택성 on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestFastlaneTests/TestFastlaneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneTests.swift
+//  TestFastlaneTests
+//
+//  Created by 이택성 on 3/14/25.
+//
+
+import Testing
+@testable import TestFastlane
+
+struct TestFastlaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
